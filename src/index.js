d2cfa7081b9eb0115f85e52edf0a6820ee78d0d9
Korotkova-Iuliia <3589900@gmail.com>
2021-11-11 00:50:34 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetch(`https://restcountries.com/v3.1/name/name`)
+//   .then(response => {
+//     response.json();
+//   })
+//   .then(name => {
+//     console.log(name);
+//   });
+
+function fetchCountries(name) {
+  const url = `https://restcountries.com/v3.1/name/name`;
+  console.log(fetch(url));
+
+  return fetch(url).then(response => response.json());
+}
+console.log(fetchCountries());
+// function renderCountry(name) {
+//   console.log(name);
+// }
+// API;
