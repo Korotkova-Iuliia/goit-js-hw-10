38b17094110ea3c672dc0d5250325248bc1c1867
Korotkova-Iuliia <3589900@gmail.com>
2021-11-12 19:02:48 +0200

    fetch

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+// function fetchCountries(name) {
+//   const url = `${BASE_URL}/name/${name}`;
+//   console.log(fetch(url));
+
+//   return fetch(url).then(response => response.json());
+// }
+// export { fetchCountries };
+// export { fetchCountries2 };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,83 @@
 import './css/styles.css';
-
+// import { fetchCountries } from './fetchCountries';
+// import { fetchCountries2 } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = `https://restcountries.com/v3.1`;
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
 
-// fetch(`https://restcountries.com/v3.1/name/name`)
-//   .then(response => {
-//     response.json();
-//   })
-//   .then(name => {
-//     console.log(name);
-//   });
-
-function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/name`;
-  console.log(fetch(url));
-
-  return fetch(url).then(response => response.json());
+inputEl.addEventListener('input', onInputEl);
+function onInputEl(e) {
+  const countryInput = inputEl.value;
+  // console.log(countryInput);
+  function fetchCountries() {
+    return fetch(`${BASE_URL}/name/${countryInput}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw Error('error 404');
+      })
+      .then(countries => {
+        console.log(countries[0]);
+        return countries;
+      });
+  }
+  console.log(fetchCountries());
 }
-console.log(fetchCountries());
-// function renderCountry(name) {
-//   console.log(name);
+
+// const markup = countries
+//   .map(({ capital }) => {
+//     return `<li class="country-info"><p>${capital}</p></li>`;
+//   })
+//   .join('');
+// countryList.insertAdjacentHTML('beforeend', markup);
+// console.log(countryList);
+
+// function renderCountries(countries) {
+//   const markup = countries
+//     .map(({ common }) => {
+//       return console.log(common);
+//       //   return `<ul class="country-list">${common}</ul>
+//       // <div class="country-info">
+//       // //  <li class="country-languages__link">${name}</li>
+//       //  </div>`;
+//     })
+//     .join('');
+//   countryList.insertAdjacentHTML('beforeend', markup);
 // }
-// API;
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//     "flags": {
+//   "svg": "https://restcountries.com/data/per.svg",
+//   "png": "https://restcountries.com/data/png/per.png"
+// }
+
+// // no рабочий код\\\\\\\\\\\\\\\\\\\\\\\\\
+// import './css/styles.css';
+// // import { fetchCountries } from './fetchCountries';
+// // import { fetchCountries2 } from './fetchCountries';
+// const DEBOUNCE_DELAY = 300;
+// const BASE_URL = `https://restcountries.com/v3.1`;
+// const inputEl = document.querySelector('#search-box');
+// const countryList = document.querySelector('country-list');
+// let countryInput = '';
+
+// inputEl.addEventListener('input', e => {
+//   const countryInput = inputEl.value;
+//   fetchCountries(countryInput).then(countries => {
+//     return countries;
+//   });
+// });
+
+// console.log(fetchCountries());
+
+// function fetchCountries(countryInput) {
+//   return fetch(`${BASE_URL}/name/${countryInput}`).then(response => {
+//     if (!response.ok) {
+//       throw Error('error 404');
+//     }
+//     return response.json();
+//   });
+// }
