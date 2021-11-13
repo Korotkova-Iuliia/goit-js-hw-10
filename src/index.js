8c06a8c54b053b9fa3689282d24c0d59c80c8e29
Korotkova-Iuliia <3589900@gmail.com>
2021-11-13 09:33:47 +0200

    markup info&name

--- src/index.js
+++ src/index.js
@@ -24,24 +24,47 @@ function fetchCountry(countryInput) {
 
 function showProfile(countries) {
   console.log(countries);
+  const markup = countries.map(country => {
+    chooseMarkup(country);
+    console.log(country);
+  });
 
-  const markupCountryInfo = countries
-    .map(({ name, flags }) => {
-      return `<div><img src="${flags.svg}" alt="flag${name.common}" height="30"/><span>${name.common}</span></div>`;
-    })
-    .join('');
-  countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
-  console.log(countryInfo);
-  const markupCountryList = countries
-    .map(({ name, capital, flags, population, languages }) => {
-      return `
-        <li class="country-item">${name.official}</li>
-        <li class=""><img src="${flags.svg}" alt="yoast seo" height="30"/></li>
-        <li class="country-item">${capital}</li>
-        <li class="country-item">${population}</li>
-        <li class="country-item">${languages}</li>`;
-    })
-    .join('');
-  countryList.insertAdjacentHTML('beforeend', markupCountryList);
-  console.log(countryList);
+  // if (countryInput === name.common) {
+
+  function chooseMarkup({ name, capital, flags, population, languages }) {
+    countryInfo.innerHTML = `<div><img src="${flags.svg}" alt="flag${name.common}" height="30"/><span>${name.common}</span></div>`;
+
+    countryList.innerHTML = `<li class="country-item">${name.official}</li>
+            <li class=""><img src="${flags.svg}" alt="yoast seo" height="30"/></li>
+            <li class="country-item">${capital}</li>
+            <li class="country-item">${population}</li>
+            <li class="country-item">${languages}</li>`;
+    // }
+  }
+
+  // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  // function chooseMarkup(country) {}
+  // countries.map(({ name }) => {
+  //   if (countryInput === name.common) {
+  //     const markupCountryList = countries
+  //       .map(({ name, capital, flags, population, languages }) => {
+  //         return `
+  //         <li class="country-item">${name.official}</li>
+  //         <li class=""><img src="${flags.svg}" alt="yoast seo" height="30"/></li>
+  //         <li class="country-item">${capital}</li>
+  //         <li class="country-item">${population}</li>
+  //         <li class="country-item">${languages}</li>`;
+  //       })
+  //       .join('');
+  //     countryList.insertAdjacentHTML('beforeend', markupCountryList);
+  //     console.log(countryList);
+  //   }
+  // });
+  // const markupCountryInfo = countries
+  //   .map(country => {
+  //     return `<div><img src="${flags.svg}" alt="flag${name.common}" height="30"/><span>${name.common}</span></div>`;
+  //   })
+  //   .join('');
+  // countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
+  // console.log(countryInfo);
 }
