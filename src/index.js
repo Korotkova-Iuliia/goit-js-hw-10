6f2b6de83d70ee673f6ef89917f1205154942dad
Korotkova-Iuliia <3589900@gmail.com>
2021-11-13 08:10:51 +0200

    markup

--- src/index.js
+++ src/index.js
@@ -5,34 +5,50 @@ const DEBOUNCE_DELAY = 300;
 const BASE_URL = `https://restcountries.com/v3.1`;
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', onInputEl);
-function onInputEl(e) {
+function onInputEl() {
   const countryInput = inputEl.value;
   // console.log(countryInput);
-  function fetchCountries() {
-    return fetch(`${BASE_URL}/name/${countryInput}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw Error('error 404');
-      })
-      .then(countries => {
-        console.log(countries[0]);
-        return countries;
-      });
-  }
-  console.log(fetchCountries());
+  fetchCountry(countryInput).then(showProfile);
 }
 
-// const markup = countries
-//   .map(({ capital }) => {
-//     return `<li class="country-info"><p>${capital}</p></li>`;
-//   })
-//   .join('');
-// countryList.insertAdjacentHTML('beforeend', markup);
-// console.log(countryList);
+function fetchCountry(countryInput) {
+  return fetch(`${BASE_URL}/name/${countryInput}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw Error('error 404');
+  });
+}
+
+function showProfile(countries) {
+  console.log(countries);
+
+  const markupCountryInfo = countries
+    .map(({ name, flags }) => {
+      return `<div><img src="${flags.svg}" alt="flag${name.common}" height="30"/><span>${name.common}</span></div>`;
+    })
+    .join('');
+  countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
+  console.log(countryInfo);
+
+  const markupCountryList = countries
+    .map(({ name, capital, flags, population, languages }) => {
+      return `
+        <li class="country-item">${name.official}</li>
+        <li class=""><img src="${flags.svg}" alt="yoast seo" height="30"/></li>
+        <li class="country-item">${capital}</li>
+        <li class="country-item">${population}</li>
+        <li class="country-item">${languages}</li>`;
+    })
+    .join('');
+  countryList.insertAdjacentHTML('beforeend', markupCountryList);
+  console.log(countryList);
+}
+
+// // function getcountry()
 
 // function renderCountries(countries) {
 //   const markup = countries
