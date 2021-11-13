452c073472a5fa003f79e825ddd5e5e2cfe62f70
Korotkova-Iuliia <3589900@gmail.com>
2021-11-13 08:19:03 +0200

    markup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
-// import { fetchCountries } from './fetchCountries';
-// import { fetchCountries2 } from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
 const BASE_URL = `https://restcountries.com/v3.1`;
 const inputEl = document.querySelector('#search-box');
@@ -33,7 +32,6 @@ function showProfile(countries) {
     .join('');
   countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
   console.log(countryInfo);
-
   const markupCountryList = countries
     .map(({ name, capital, flags, population, languages }) => {
       return `
@@ -47,53 +45,3 @@ function showProfile(countries) {
   countryList.insertAdjacentHTML('beforeend', markupCountryList);
   console.log(countryList);
 }
-
-// // function getcountry()
-
-// function renderCountries(countries) {
-//   const markup = countries
-//     .map(({ common }) => {
-//       return console.log(common);
-//       //   return `<ul class="country-list">${common}</ul>
-//       // <div class="country-info">
-//       // //  <li class="country-languages__link">${name}</li>
-//       //  </div>`;
-//     })
-//     .join('');
-//   countryList.insertAdjacentHTML('beforeend', markup);
-// }
-
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-//     "flags": {
-//   "svg": "https://restcountries.com/data/per.svg",
-//   "png": "https://restcountries.com/data/png/per.png"
-// }
-
-// // no рабочий код\\\\\\\\\\\\\\\\\\\\\\\\\
-// import './css/styles.css';
-// // import { fetchCountries } from './fetchCountries';
-// // import { fetchCountries2 } from './fetchCountries';
-// const DEBOUNCE_DELAY = 300;
-// const BASE_URL = `https://restcountries.com/v3.1`;
-// const inputEl = document.querySelector('#search-box');
-// const countryList = document.querySelector('country-list');
-// let countryInput = '';
-
-// inputEl.addEventListener('input', e => {
-//   const countryInput = inputEl.value;
-//   fetchCountries(countryInput).then(countries => {
-//     return countries;
-//   });
-// });
-
-// console.log(fetchCountries());
-
-// function fetchCountries(countryInput) {
-//   return fetch(`${BASE_URL}/name/${countryInput}`).then(response => {
-//     if (!response.ok) {
-//       throw Error('error 404');
-//     }
-//     return response.json();
-//   });
-// }
